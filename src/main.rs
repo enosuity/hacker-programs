5192c1ff33319868a5abde50c3379aef23aae25d
Anuj <anuj.dhiman@antiersolutions.com>
2024-06-11 17:14:55 +0530

    Changes in metadata

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,27 @@
-#![warn(dead_code)]
-use std::{i32, io::{self, BufRead}};
+use minisearch::run;
+use std::{env, process};
+
 
 
 fn main() {
-       
+    // =========== using string vector =============
+    // let args: Vec<String> = env::args().collect();
+
+    // let config = minigrep::Config::build(args).unwrap_or_else(|err| {
+    //     eprintln!("Problem parsing arguments: {}", err);
+    //     process::exit(1);
+    // });    
+
+    // ============= same thing we can do using Iterator ===============    
+    let config = minisearch::Config::build(env::args()).unwrap_or_else(|err| {
+        eprintln!("Problem parsing arguments: {}", err);
+        process::exit(1);
+    });
+
+
+    run(config).unwrap_or_else(|err|{
+        eprintln!("Application error: {err}");
+        process::exit(1);
+    });
+    
 }
